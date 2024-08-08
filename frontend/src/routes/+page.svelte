5c866f9471640868f6fd10509595ead780ecf97d
helenamartins81 <lenamartiins@gmail.com>
2024-08-08 15:24:52 +0100

    Display url page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -28,7 +28,7 @@
   let secretText = "";
   let fileinput;
   let images = [];
-  let imageBase64Strings = []; // To store the base64 strings
+  let imageBase64Strings = [];
 
   let encryptedText: string;
   let encryptionKey: string;
@@ -56,7 +56,6 @@
         const promises = images.map((file) => convertFileToBase64(file));
         imageBase64Strings = await Promise.all(promises);
         await progress.set(0.4);
-        console.log("Base64 Encoded Files:", imageBase64Strings);
       }
 
       const combinedData = {
@@ -64,42 +63,38 @@
         images: imageBase64Strings,
       };
       const combinedString = JSON.stringify(combinedData);
-      console.log("Combined Data:", combinedString);
 
       await progress.set(0.5);
       encryptionKey = generatePassphrase();
       encryptedText = await encryptData(combinedData, encryptionKey);
       await progress.set(0.7);
-      console.log("encryptedText -> ", encryptedText);
 
       roomId = await createSecret({
         secret: encryptedText,
         expiry: Number(expiry),
       });
       await progress.set(0.9);
-
       sharingUrl = `${location.protocol}//${location.host}/${roomId}#${encryptionKey}`;
-      console.log("sharing url ", sharingUrl);
 
       await progress.set(1);
       submitting = false;
     } catch (error) {
       console.error("Error:", error);
-      // Handle or display the error appropriately
       // goto("/error");
       submitting = false;
     }
   }
 </script>
 
-{#if !submitting}
+<!-- svelte-ignore empty-block -->
+{#if !submitting && !sharingUrl}
   <div class="page-container">
     <p align="center">
       Share information securely and ephemerally. <br />The generated link will
       only work once, then it will disappear forever.
     </p>
     <p align="center">How it works?</p>
-    <div align="center">
+    <div align="center" class="container">
       <div>
         <textarea
           bind:value={secretText}
@@ -144,6 +139,22 @@
       </div>
     </div>
   </div>
+{:else if !submitting && sharingUrl}
+  <div class="page-container">
+    <div align="center">
+      <p>
+        Your secret is ready. Keep in mind the link can only be revealed once,
+        then it is utterly destroyed.
+      </p>
+      <div class="secretLink">
+        <a href={sharingUrl} class="url">{sharingUrl}</a>
+      </div>
+      <div class="copyLink">
+        <Button class="lg sec" on:click={() => {}}>Reset</Button>
+        <Button class="lg primary">Copy link</Button>
+      </div>
+    </div>
+  </div>
 {:else}
   <div class="page-container">
     <div align="center">
@@ -154,7 +165,9 @@
       <p>How it works?</p>
       <div class="loading">
         <p class="encryptingText">Encrypting your secret...</p>
-        <progress value={$progress} max="1" />
+        <div align="left">
+          <progress value={$progress} max="1"></progress>
+        </div>
       </div>
     </div>
   </div>
@@ -170,6 +183,7 @@
     font-weight: 400;
     line-height: 28px;
     text-align: center;
+    font-family: "Inter", sans-serif;
   }
   textarea {
     width: 100%;
@@ -195,33 +209,26 @@
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.16);
     background-color: white;
     padding: 60px;
-    resize: none;
   }
+
   progress {
-    display: block;
     width: 100%;
     max-width: 880px;
     height: 16px;
     border-radius: 24px;
   }
   progress::-webkit-progress-bar {
-    background: #f3f3f3; 
-    border-radius: 24px;
-  }
-
-  progress::-webkit-progress-value {
-    background: linear-gradient(
-      90deg,
-      #2c8dff 0%,
-      #0263f4 100%
-    ); 
+    background: #f3f3f3;
     border-radius: 24px;
   }
   progress::-moz-progress-bar {
-    background: linear-gradient(90deg, #2C8DFF 0%, #0263F4 100%);
+    background: linear-gradient(90deg, #2c8dff 0%, #0263f4 100%);
     border-radius: 24px;
-}
-
+  }
+  progress::-webkit-progress-value {
+    background: linear-gradient(90deg, #2c8dff 0%, #0263f4 100%);
+    border-radius: 24px;
+  }
   .actions {
     display: flex;
     flex-wrap: wrap;
@@ -288,5 +295,43 @@
     font-weight: 600;
     line-height: 24px;
     color: #045cfc;
+    margin-bottom: 10px;
+  }
+  .secretLink {
+    width: 100%;
+    max-width: 880px;
+    height: 112px;
+    gap: 0px;
+    border-radius: 12px;
+    background: white;
+    display: flex; /* Use Flexbox for alignment */
+    justify-content: center; /* Center horizontally */
+    align-items: center; /* Center vertically if needed */
+    text-align: center; /* Ensure the text is centered */
+  }
+
+  .secretLink .url {
+    display: inline-block; /* Ensures the link respects the centering */
+    word-wrap: break-word; /* Optional: Break the link if it's too long */
+  }
+
+  .url {
+    width: 880px;
+    font-family: "Inter", sans-serif;
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 32px;
+    text-align: center;
+    margin: auto;
+  }
+  .copyLink{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    max-width: 880px;
+    padding: 30px;
+
   }
 </style>
