e24993d11012afa27ce0b3d4c88dd9e38d129aa7
Davide Silva <davide@subvisual.co>
2025-06-25 12:05:19 +0100

    remove config for static manifest

--- api/config/prod.exs
+++ api/config/prod.exs
@@ -2,7 +2,6 @@ import Config
 
 config :secrets_api, SecretsApiWeb.Endpoint,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   check_origin: false
 
 config :logger, level: :info
